d9be9206cad605fcf55a82f390e92d75176b24c8
jg229 <joeguthrie2@hotmail.com>
2018-09-18 17:31:23 +0100

    added physics engine

--- src/tanksgame/Tank.java
+++ src/tanksgame/Tank.java
@@ -1,55 +1,55 @@
-package tanksgame;
-
-public class Tank {
-	int xpos, ypos;
-	boolean drivingLeft = false;
-	boolean drivingRight = false;
-	boolean rotateLeft = false;
-	boolean rotateRight = false;
-	
-	private static final int movingSpeed = 2;
-	private static final double rotationSpeed = 0.01;
-	double rotateCount = 0.0;
-	
-	double firingAngle;
-	double firingLimit = Math.PI/10;
-	
-	Tank(int xpos, int ypos, double firingAngle){
-		this.xpos = xpos;
-		this.ypos = ypos;
-		this.firingAngle = firingAngle;
-	}
-  
-	int getXpos(){
-		return xpos;
-	}
-  
-	int getYpos(){
-		return ypos; 
-	}
-	
-	double getFiringAngle() {
-		System.out.println(firingAngle);
-		return firingAngle;
-	}
-	
-	void drive() {
-		if(drivingLeft) {
-			xpos -= movingSpeed;
-		}
-		
-		if(drivingRight) {
-			xpos += movingSpeed;
-		}
-	}
-	
-	void rotateCannon() {
-		if(rotateLeft && firingAngle < (Math.PI + firingLimit)) {
-			firingAngle += rotationSpeed;
-		}
-		
-		if(rotateRight && firingAngle > (0 - firingLimit)) {
-			firingAngle -= rotationSpeed;
-		}
-	}
-}
+//package tanksgame;
+//
+//import processing.core.PVector;
+//
+//public class Tank {
+//	PVector position;
+//	boolean drivingLeft = false;
+//	boolean drivingRight = false;
+//	boolean rotateLeft = false;
+//	boolean rotateRight = false;
+//	
+//	private static final int movingSpeed = 2;
+//	private static final double rotationSpeed = 0.01;
+//	double rotateCount = 0.0;
+//	
+//	float firingAngle;
+//	float firingLimit = (float) (Math.PI/10);
+//	
+//	Tank(int xpos, int ypos, float firingAngle){
+//		this.position = new PVector(xpos, ypos);
+//		this.firingAngle = firingAngle;
+//	}
+//  
+//	float getXpos(){
+//		return position.x;
+//	}
+//  
+//	float getYpos(){
+//		return position.y; 
+//	}
+//	
+//	float getFiringAngle() {
+//		return firingAngle;
+//	}
+//	
+//	void drive() {
+//		if(drivingLeft) {
+//			position.x -= movingSpeed;
+//		}
+//		
+//		if(drivingRight) {
+//			position.x += movingSpeed;
+//		}
+//	}
+//	
+//	void rotateCannon() {
+//		if(rotateLeft && firingAngle < (Math.PI + firingLimit)) {
+//			firingAngle += rotationSpeed;
+//		}
+//		
+//		if(rotateRight && firingAngle > (0 - firingLimit)) {
+//			firingAngle -= rotationSpeed;
+//		}
+//	}
+//}
